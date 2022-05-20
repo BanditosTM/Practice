52f36b41859e21d6565c4e3f2608c45e99a852ba
BanditosTM <74076655+BanditosTM@users.noreply.github.com>
2022-05-20 13:45:50 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open("static")
+        .then(function(cache) {
+            console.log("precaching");
+            cache.add('/index.html');
+            cache.add('/');
+        })
+    )
+});
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if(response)
+                    return response;
+                else
+                    return fetch(event.request);
+            })
+    )
+});
+
